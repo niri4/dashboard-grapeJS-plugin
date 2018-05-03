11189dfee2787ac89a9fcb036b9a1b933e76ba27
Maropost India <nirankarsingh247@gmail.com>
2018-05-04 00:50:01 +0530

    add chart js

--- src/blocks.js
+++ src/blocks.js
@@ -1545,5 +1545,70 @@ bm.add('button', {
     attributes: { class: 'btn' },
     content: button,
 });
+
+
+
+// *************************************************************************************graphs***********************************************************************************************************************************
+let line_graphs =`
+<canvas id="myChart" width="400" height="400"></canvas>
+<script>
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+</script>`;
+
+bm.add('line_graph', {
+    category: 'Graphs',
+    label: `line_graph`,
+    attributes: { class: 'btn' },
+    content: {
+    type: 'canvas',
+    activeOnRender: 1,
+    components: line_graphs // <- this will trigger the active event
+ },
+    command: 'myCommand'
+});
+
+// commands.add('myCommand', {
+//   run:  function(editor, sender){
+//     alert('Hello world!');
+//   },
+//   stop:  function(editor, sender){
+//   },
+// });
   // ...
 }
--- src/components.js
+++ src/components.js
@@ -3,5 +3,55 @@ export default (editor, config = {}) => {
   const defaultType = domc.getType('default');
   const defaultModel = defaultType.model;
   const defaultView = defaultType.view;
+
+
+
+  // The `input` will be the Component type ID
+domc.addType('canvas', {
+  // Define the Model
+  model: defaultModel.extend({
+    // Extend default properties
+    defaults: Object.assign({}, defaultModel.prototype.defaults, {
+      // Can be dropped only inside `form` elements
+      // draggable: 'div, div*',
+      // // Can't drop other elements inside it
+      // droppable: false,
+      // Traits (Settings)
+      // traits: ['name', 'placeholder', {
+      //     // Change the type of the input (text, password, email, etc.)
+      //     type: 'select',
+      //     label: 'Type',
+      //     name: 'type',
+      //     options: inputTypes,
+      //   },{
+      //     // Can make it required for the form
+      //     type: 'checkbox',
+      //     label: 'Required',
+      //     name: 'required',
+      // }],
+    }),
+  },
+  // The second argument of .extend are static methods and we'll put inside our
+  // isComponent() method. As you're putting a new Component type on top of the stack,
+  // not declaring isComponent() might probably break stuff, especially if you extend
+  // the default one.
+  {
+    isComponent: function(el) {
+      if(el.tagName == 'CANVAS'){
+        return {type: 'canvas'};
+      }
+    },
+  }),
+
+  // Define the View
+  view: defaultType.view.extend({
+    init() {
+         this.listenTo(this.model, 'active', this.doStuff); // listen for active event
+       },
+       doStuff() {alert(2);}
+
+
+  }),
+});
   // ...
 }
